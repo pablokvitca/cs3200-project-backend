dca0da39a14fe2672f596c3542793be152418b04
Pablo Kvitca <pablokvitca@gmail.com>
2019-03-30 21:30:35 -0400

    Added session secured endpoints to pursued_degree controller

--- swagger_server/controllers/pursued_degree_controller.py
+++ swagger_server/controllers/pursued_degree_controller.py
@@ -4,6 +4,13 @@ import six
 from swagger_server.models.pursued_degree import PursuedDegree  # noqa: E501
 from swagger_server import util
 
+from swagger_server import encoder
+
+from sqlalchemy import types
+from sqlalchemy import exc
+
+from flask import make_response
+
 
 def add_pursued_degree(body):  # noqa: E501
     """Add a pursued_degree to the classdeck
@@ -17,7 +24,27 @@ def add_pursued_degree(body):  # noqa: E501
     """
     if connexion.request.is_json:
         body = PursuedDegree.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
+        insert_string = """
+            INSERT INTO pursued_degree (
+                NUID,
+                degree_ID)
+            VALUES (
+                "{0}",
+                {1});
+            """.format(body.nuid, body.degree_id)
+        try:
+            session_cookie = connexion.request.cookies.get("session")
+            session_NUID = connexion.verify_JWT(session_cookie)
+            if (session_NUID == body.nuid):
+                connexion.DB.execute(insert_string)
+                return "Accepted", 201
+            else:
+                return "Forbidden", 403
+        except exc.IntegrityError:
+            return "Could not add pursued degree", 406
+        except KeyError:
+            return "Forbidden", 403
+    return "Bad Request", 400
 
 
 def delete_pursued_degree(nuid, degree_id):  # noqa: E501
@@ -32,7 +59,28 @@ def delete_pursued_degree(nuid, degree_id):  # noqa: E501
 
     :rtype: None
     """
-    return 'do some magic!'
+    if connexion.request.is_json:
+        body = PursuedDegree.from_dict(connexion.request.get_json())  # noqa: E501
+        delete_string = """
+            DELETE FROM pursued_degree
+            WHERE
+                NUID = {0},
+                AND
+                degree_id = {1};
+            """.format(body.nuid, body.degree_id)
+        try:
+            session_cookie = connexion.request.cookies.get("session")
+            session_NUID = connexion.verify_JWT(session_cookie)
+            if (session_NUID == body.nuid):
+                connexion.DB.execute(delete_string)
+                return "Accepted", 201
+            else:
+                return "Forbidden", 403
+        except exc.IntegrityError:
+            return "Could not add pursued degree", 406
+        except KeyError:
+            return "Forbidden", 403
+    return "Bad Request", 400
 
 
 def get_pursued_degree_by_id(nuid, degree_id):  # noqa: E501
