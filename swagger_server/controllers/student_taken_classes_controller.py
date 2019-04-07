baa5652ad9fd0b2b7cff4e584db8c99aac0b378b
Pablo Kvitca <pablokvitca@gmail.com>
2019-04-07 19:00:03 -0400

    inprogress

--- swagger_server/controllers/student_taken_classes_controller.py
+++ swagger_server/controllers/student_taken_classes_controller.py
@@ -37,7 +37,7 @@ def delete_student_taken_classes(nuid, class_dept, class_number):  # noqa: E501
     return 'do some magic!'
 
 
-def get_student_taken_classes_by_id(nuid, class_dept, class_number):  # noqa: E501
+def get_student_taken_classes_by_nuid(nuid):  # noqa: E501
     """Find student_taken_classes by ID
 
     Returns a single student_taken_classes # noqa: E501
@@ -51,7 +51,27 @@ def get_student_taken_classes_by_id(nuid, class_dept, class_number):  # noqa: E5
 
     :rtype: StudentTakenClasses
     """
-    return 'do some magic!'
+    select_string = """
+            SELECT * FROM classes_taken
+            WHERE
+                nuid = {};
+            """.format(nuid)
+    try:
+        session_cookie = connexion.request.cookies.get("session")
+        session_NUID = connexion.JWT_verify(session_cookie)
+        if session_NUID == str(nuid).zfill(9):
+            result = connexion.DB.execute(select_string)
+            res = []
+            for nuid, class_dept, class_number in result.fetchall():
+                r = StudentTakenClasses(nuid, degree_id)
+                res.append(r)
+            return res, 201
+        else:
+            return "Forbidden", 403
+    except exc.IntegrityError as err:
+        return "Could not add pursued degree", 406
+    except KeyError:
+        return "Forbidden", 403
 
 
 def update_student_taken_classes(body):  # noqa: E501
